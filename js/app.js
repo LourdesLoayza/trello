263772e956fd5217c9ab9a0b80cf23700ca33190
Gabriel Loayza Mesias <loayza.lourdes@gmail.com>
2017-11-19 14:59:14 -0500

    crando file

---
+++ js/app.js
@@ -0,0 +1,47 @@
+
+// Versión 0.0.1 ----------------------------------------
+
+window.addEventListener('load', function () {
+  // var insertTweets= document.getElementById('insert-tweets');
+  // var btnSave = document.getElementById('save');
+  
+  // var showSize = document.getElementById('string-size');
+  
+  var listContainer = document.getElementById('list-container');
+  var hideText = document.getElementById('hide-text');
+
+  hideText.addEventListener('click', function() {
+
+    hideText.textContent = '';
+
+  
+        
+  }); 
+
+ 
+
+
+  
+  // ocultar texto mostrar formulario
+  // function showMenu(text) {
+
+  //   var newTweet = document.createElement('p');
+  //   newTweet.setAttribute('class','tweet')
+  //   newTweet.textContent = text;
+  //   containerTweets.insertBefore(newTweet, containerTweets.childNodes[0]);
+  //   insertTweets.value='';   
+    
+  //   var insertTime = document.createElement('p');
+  //   insertTime.setAttribute('class','time');
+  //   insertTime.textContent = horactual;
+  //   containerTweets.insertBefore(insertTime,containerTweets.childNodes[1]);
+
+
+  // }
+
+  // btnSave.addEventListener('click', function() {
+  //   showMenu(insertTweets.value);
+  //   addTime();
+  // }); 
+
+}); 
