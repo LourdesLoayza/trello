dc72b8f38107ba34c7eb30bc65dab0267148384f
LourdesLoayza <30554218+LourdesLoayza@users.noreply.github.com>
2017-11-19 23:45:11 -0500

    Add files via upload
    avance al 60%

--- js/app.js
+++ js/app.js
@@ -2,46 +2,62 @@
 // Versión 0.0.1 ----------------------------------------
 
 window.addEventListener('load', function () {
-  // var insertTweets= document.getElementById('insert-tweets');
-  // var btnSave = document.getElementById('save');
-  
-  // var showSize = document.getElementById('string-size');
-  
-  var listContainer = document.getElementById('list-container');
-  var hideText = document.getElementById('hide-text');
+  var insertList= document.getElementById('insert-list');
+  var btnNewLIst = document.getElementById('new-list');
+  var containerListOne= document.getElementById('container-list-one');
+  var containerListTwo= document.getElementById('container-list-two');
+  var btnSaveTask = document.getElementById('btn-new-list')
 
-  hideText.addEventListener('click', function() {
-
-    hideText.textContent = '';
-
+  var newTask = document.createElement('textarea');
+  var btnSaveTask = document.createElement('button');
+  var nameList = document.createElement('ul');
+  var newSubTask = document.createElement('li');
+    
   
-        
+  // mostrar formulario para agregar lista de actividades
+  function addContainerOne(text) {   
+    btnSaveTask.textContent = 'Guardar';
+    newTask.setAttribute ('placeholder', 'Añade una lista...');
+    btnSaveTask.setAttribute('id','btn-new-list')
+    containerListOne.style.background = ('#e2e4e6');
+    
+    containerListOne.insertBefore(newTask, containerListOne.childNodes[0]);
+    containerListOne.insertBefore(btnSaveTask, containerListOne.childNodes[1]);
+  }
+
+  // mostrar formulario para agregar sub lista de actividades
+  function addContainerTwo(text) {   
+    btnSaveTask.textContent = 'Guardar';
+    newTask.setAttribute ('placeholder', 'Añade una lista...');
+    btnSaveTask.setAttribute('id','btn-new-list')
+    containerListTwo.style.background = ('#e2e4e6');
+    nameList.textContent = newTask.value;
+    
+    newSubTask.setAttribute('id','new-sub-task')
+    newSubTask.textContent = 'Añadir una tarea'
+
+    containerListTwo.insertBefore(newTask, containerListTwo.childNodes[0]);
+    containerListTwo.insertBefore(btnSaveTask, containerListTwo.childNodes[1]);
+    containerListOne.insertBefore(nameList, containerListOne.childNodes[0]);
+    containerListOne.insertBefore(newSubTask, containerListOne.childNodes[1]);
+  }
+
+  // muestra formulario para agregar lista de actividades
+  btnNewLIst.addEventListener('click', function() {
+    btnNewLIst.parentNode.removeChild(btnNewLIst);
+    addContainerOne(btnNewLIst.value); 
+  }); 
+  
+  // mostrar formulario genera para nuevas actividades
+  btnSaveTask.addEventListener('click', function() {
+    btnSaveTask.parentNode.removeChild(btnSaveTask);
+    newTask.parentNode.removeChild(newTask);
+    addContainerTwo(btnSaveTask.value);
+    newTask.value = '';
   }); 
 
- 
-
-
   
-  // ocultar texto mostrar formulario
-  // function showMenu(text) {
-
-  //   var newTweet = document.createElement('p');
-  //   newTweet.setAttribute('class','tweet')
-  //   newTweet.textContent = text;
-  //   containerTweets.insertBefore(newTweet, containerTweets.childNodes[0]);
-  //   insertTweets.value='';   
-    
-  //   var insertTime = document.createElement('p');
-  //   insertTime.setAttribute('class','time');
-  //   insertTime.textContent = horactual;
-  //   containerTweets.insertBefore(insertTime,containerTweets.childNodes[1]);
 
 
-  // }
 
-  // btnSave.addEventListener('click', function() {
-  //   showMenu(insertTweets.value);
-  //   addTime();
-  // }); 
-
-}); 
+});  
